60d66fdb85984dace005cb63eaa0eb208efa24a6
b1 <alch3mist94@protonmail.com>
2021-11-15 00:25:58 +0100

    added a reboot loop

--- w2l.sh
+++ w2l.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-# W2L
-# This script sets up a remote desktop protocol (RDP) server xrdp persistently
+# --------------------------------------------------------------------------------------------------------
+# W2L                                                                                                      
+# This script sets up a remote desktop protocol (RDP) server persistently.
 # The login info is printed aftersetup and need to be inserted in Windows 'Remote Desktop Connection' app
-
-
+# --------------------------------------------------------------------------------------------------------
 function caddy () {
     local pid=$!
     declare -a ani=("   🛒" "  🛒 " " 🛒  " "🛒   " "     ")
@@ -18,25 +17,29 @@ function caddy () {
         sleep .15
     done
 }
-
 function highlight () {
 	printf "\n\t\033[1;33m$1\033[1;35m\n"; sleep 1
 }
-
 function install () {
     eval "sudo apt install $1 -y" &> /dev/null & caddy "installing $1"
 }
-
 highlight "W2L"
 highlight "Installing Dependencies ..."
 sudo apt-get update &&
 install ssh &&
 install xrdp &&
-
 highlight "Enable to start after reboot and run the remote desktop sharing server ..."
 sudo systemctl enable --now xrdp
 highlight "Set TCP port ..."
 sudo ufw allow from any to any port 3389 proto tcp
 highlight "done."
-
 highlight "This host will be reachable through Windows RDP, connect via:\nUser: $(whoami)\nHost: $(hostname)"
+highlight "Reboot system now [recommended]? (y/n)"
+read qreboot
+if [ $qreboot == "y" ]; then
+	for i in 5 4 3 2 1
+	do
+		printf "Reboot in $i seconds ...\r"; sleep 1
+	done
+	sudo reboot
+fi
